651b67c70ab691a3aa85edc0e3062ba170a1e6e2
henrique <henriquim25@yahoo.com.br>
2023-08-11 00:54:08 -0300

    database created

---
+++ src/auth/auth.service.ts
@@ -0,0 +1,12 @@
+import { Injectable } from '@nestjs/common';
+import { User, Bookmark } from '@prisma/client';
+
+@Injectable({})
+export class AuthService {
+  signin() {
+    return 'I am sign in';
+  }
+  signup() {
+    return 'I am sign up';
+  }
+}
